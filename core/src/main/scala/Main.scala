0f36ba3e0eb692096e0868aeee3cf823729d9747
DmytroMitin <dmitin3@gmail.com>
2022-11-11 13:28:43 +0200

    Add Main

---
+++ core/src/main/scala/Main.scala
@@ -0,0 +1,30 @@
+import io.circe.{Codec, Decoder, Encoder}
+import io.circe.generic.semiauto.deriveCodec
+import io.circe.syntax._
+
+//shapeless: KeyTag, type
+//circe: DerivationMacros, KeyTag
+//https://stackoverflow.com/questions/74384745/encoding-decoding-a-field-with-any-datatype-assigned-with-value-none-in-scala
+object Main extends App {
+  case class Demo(
+                   field1: Any
+                 )
+
+  val myDemo=Demo(field1=None)
+  print(myDemo.asJson+"\n")
+
+  implicit val valueEncoderValue: Encoder[Any] = Encoder.encodeString.contramap[Any](x=>{
+    x.toString})
+
+  implicit val valueDecoderValue: Decoder[Any] = Decoder.decodeString.map[Any](x => {
+    if (x == "Any")
+      x.asInstanceOf[Any]
+    else
+      x.toString
+  })
+
+  //java.lang.ClassCastException: scala.None$ cannot be cast to shapeless.labelled$KeyTag
+  implicit lazy val DemoCodec: Codec[Demo] =
+    deriveCodec[Demo]
+
+}
